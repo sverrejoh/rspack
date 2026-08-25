8527140c630424b81233a1856ebf446f69cf3db5
harpsealjs <lingyucoder@gmail.com>
2026-08-25 17:13:25 +0800

    refactor(javascript): migrate parser to swc next

--- crates/rspack_plugin_javascript/src/parser_plugin/require_context_dependency_parser_plugin.rs
+++ crates/rspack_plugin_javascript/src/parser_plugin/require_context_dependency_parser_plugin.rs
@@ -4,7 +4,7 @@ use rspack_core::{
 };
 use rspack_regex::RspackRegex;
 use rspack_util::SpanExt;
-use swc_experimental_ecma_ast::{CallExpr, GetSpan};
+use swc_next_ecma_ast::{CallExpression, GetSpan};
 
 use super::JavascriptParserPlugin;
 use crate::{
@@ -19,15 +19,17 @@ impl<'p, 'a> JavascriptParserPlugin<'p, 'a> for RequireContextDependencyParserPl
   fn call(
     &self,
     parser: &mut JavascriptParser<'p>,
-    expr: &CallExpr,
+    expr: CallExpression,
     for_name: &str,
   ) -> Option<bool> {
     if for_name != "require.context" {
       return None;
     }
+    let ast = parser.ast.ast;
+    let arguments = expr.arguments(ast);
 
-    let mode = if expr.args.len() == 4 {
-      let mode_expr = parser.evaluate_expression(&expr.args[3].expr);
+    let mode = if arguments.len() == 4 {
+      let mode_expr = parser.evaluate_expression(arguments.get_node(ast, 3)?.as_expr(ast)?);
       if !mode_expr.is_string() {
         // FIXME: return `None` in webpack
         ContextMode::Sync
@@ -40,8 +42,9 @@ impl<'p, 'a> JavascriptParserPlugin<'p, 'a> for RequireContextDependencyParserPl
       ContextMode::Sync
     };
 
-    let (reg_exp, reg_exp_span) = if expr.args.len() >= 3 {
-      let reg_exp_expr = parser.evaluate_expression(&expr.args[2].expr);
+    let (reg_exp, reg_exp_span) = if arguments.len() >= 3 {
+      let argument = arguments.get_node(ast, 2)?.as_expr(ast)?;
+      let reg_exp_expr = parser.evaluate_expression(argument);
       let reg_exp = if !reg_exp_expr.is_regexp() {
         // FIXME: return `None` in webpack
         default_context_reg_exp()
@@ -49,13 +52,13 @@ impl<'p, 'a> JavascriptParserPlugin<'p, 'a> for RequireContextDependencyParserPl
         let (expr, flags) = reg_exp_expr.regexp();
         RspackRegex::with_flags(expr.as_str(), flags.as_str()).expect("reg should success")
       };
-      (reg_exp, Some(expr.args[2].expr.span().into()))
+      (reg_exp, Some(argument.span(ast).into()))
     } else {
       (default_context_reg_exp(), None)
     };
 
-    let recursive = if expr.args.len() >= 2 {
-      let recursive_expr = parser.evaluate_expression(&expr.args[1].expr);
+    let recursive = if arguments.len() >= 2 {
+      let recursive_expr = parser.evaluate_expression(arguments.get_node(ast, 1)?.as_expr(ast)?);
       if !recursive_expr.is_bool() {
         // FIXME: return `None` in webpack
         true
@@ -66,8 +69,8 @@ impl<'p, 'a> JavascriptParserPlugin<'p, 'a> for RequireContextDependencyParserPl
       true
     };
 
-    if let Some(arg) = expr.args.first() {
-      let request_expr = parser.evaluate_expression(&arg.expr);
+    if let Some(arg) = arguments.get_node(ast, 0).and_then(|arg| arg.as_expr(ast)) {
+      let request_expr = parser.evaluate_expression(arg);
       if !request_expr.is_string() {
         return None;
       }
@@ -82,11 +85,11 @@ impl<'p, 'a> JavascriptParserPlugin<'p, 'a> for RequireContextDependencyParserPl
           request: request_expr.string().clone(),
           context: get_context(parser.resource_data).to_string(),
           compiler_context: parser.compiler_options.context.clone(),
-          start: expr.span().real_lo(),
-          end: expr.span().real_hi(),
+          start: expr.span(ast).real_lo(),
+          end: expr.span(ast).real_hi(),
           ..Default::default()
         },
-        expr.span.into(),
+        expr.span(ast).into(),
         parser.in_try,
       )));
       return Some(true);
